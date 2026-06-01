60d99b3b45663af96be4fcbe245646ebfb79a608
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-06-01 12:54:30 +0900

    Update level_editor.py
    TL1_01_04

--- level_editor.py
+++ level_editor.py
@@ -13,11 +13,43 @@ bl_info = {
     "category" : "Object",
 }
 
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    
+    bl_label = "MyMenu"
+    
+    bl_description = "拡張メニュー by" + bl_info["author"]
+    
+    def draw(self, context):
+        self.layout.operator("wm.url_open_preset", 
+            text="Manual", icon="HELP")
+
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
+        
+
+def draw_menu_manual(self,context): 
+    self.layout.operator("wm.url_open_preset", text="Manual", icon="HELP")
+    
 def register():
-    print("レベルエディター enabled")
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("レベルエディターが有効化されました")
     
 def unregister():
-    print("レベルエディター disabled")
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    print("レベルエディターが無効化されました")
     
 if __name__ ==  "__main__" : 
     register()
+    
