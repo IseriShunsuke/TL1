3567d94b7f844d8daa0f1c0341ce8bff3920c384
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-06-10 17:44:46 +0900

    TL1_01_05
    TL1_01_05

--- level_editor.py
+++ level_editor.py
@@ -13,6 +13,21 @@ bl_info = {
     "category" : "Object",
 }
 
+class MYADDON_OT_strech_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_strech_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    bl_options = {'REGISTER','UNDO'}
+
+    def execute(self,context): 
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました")
+
+        return {'FINISHED'}
+
+
+
+
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     bl_idname = "TOPBAR_MT_my_menu"
     
@@ -23,14 +38,17 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     def draw(self, context):
         self.layout.operator("wm.url_open_preset", 
             text="Manual", icon="HELP")
+        self.layout.operator(MYADDON_OT_strech_vertex.bl_idname, 
+            text=MYADDON_OT_strech_vertex.bl_label)
+
 
     def submenu(self, context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 classes = (
+    MYADDON_OT_strech_vertex,
     TOPBAR_MT_my_menu,
 )
-
         
 
 def draw_menu_manual(self,context): 
