528257f95bdc3d5a30873fc00134852bfc1fe36d
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-06-10 19:33:16 +0900

    TL1_01_06
    TL1_01_06

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 bl_info = {
     "name" : "レベルエディター",
@@ -12,6 +13,45 @@ bl_info = {
     "tracker_url" : "",
     "category" : "Object",
 }
+
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self,context):
+         
+         print("シーン情報をExportします")
+
+         for object in bpy.context.scene.objects:
+            print(object.type + "-" + object.name)
+        
+            trans, rot, scale = object.matrix_local.decompose()
+
+            rot = rot.to_euler()
+
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+
+            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x,rot.y,rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x,scale.y,scale.z))
+
+            if object.parent:
+                print("Parent: " + object.parent.name)
+            print()
+
+
+
+         print("シーン情報をExportしました")
+         self.report({'INFO'},"シーン情報をExportしました")
+
+         return {'FINISHED'}
+
+
+
+
 
 class MYADDON_OT_strech_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_strech_vertex"
@@ -40,12 +80,15 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
             text="Manual", icon="HELP")
         self.layout.operator(MYADDON_OT_strech_vertex.bl_idname, 
             text=MYADDON_OT_strech_vertex.bl_label)
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, 
+            text=MYADDON_OT_export_scene.bl_label)
 
 
     def submenu(self, context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 classes = (
+    MYADDON_OT_export_scene,
     MYADDON_OT_strech_vertex,
     TOPBAR_MT_my_menu,
 )
