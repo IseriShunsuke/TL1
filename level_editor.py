374caa10ee4d5685ba59b92c30d717465a1ea6a6
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-06-16 18:22:14 +0900

    TL1_01_07
    TL1_01_07

--- level_editor.py
+++ level_editor.py
@@ -1,5 +1,6 @@
 import bpy
 import math
+import bpy_extras
 
 bl_info = {
     "name" : "レベルエディター",
@@ -14,35 +15,64 @@ bl_info = {
     "category" : "Object",
 }
 
-class MYADDON_OT_export_scene(bpy.types.Operator):
+class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     bl_idname = "myaddon.myaddon_ot_export_scene"
     bl_label = "シーン出力"
     bl_description = "シーン情報をExportします"
+    filename_ext = ".scene"
+
+    def parse_scene_recursive(self, file, object, level):
+        """シーン回帰関数"""
+
+        indent = ''
+        for i in range(level):
+            indent += "\t"
+
+       
+        self.write_and_print(file, indent +  object.type +" - "+ object.name)
+        trans, rot, scale = object.matrix_local.decompose()
+
+        rot = rot.to_euler()
+
+        rot.x = math.degrees(rot.x)
+        rot.y = math.degrees(rot.y)
+        rot.z = math.degrees(rot.z)
+
+        self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+        self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot.x,rot.y,rot.z))
+        self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x,scale.y,scale.z))
+        self.write_and_print(file,'')
+
+        for child in object.children:
+            self.parse_scene_recursive(file,child,level + 1)
+
+    def write_and_print(self, file, str):
+        print(str)
+
+        file.write(str)
+        file.write('\n')
+
+    def export(self):
+        """ファイルに出力"""
+
+        print("シーン情報出力開始…%r" % self.filepath)
+
+        with open(self.filepath, "wt") as file:
+            file.write("SCENE\n")
+
+            for object in bpy.context.scene.objects:
+                if object.parent:
+                    continue
+                
+                self.parse_scene_recursive(file, object, 0)
+
+            
 
     def execute(self,context):
          
          print("シーン情報をExportします")
 
-         for object in bpy.context.scene.objects:
-            print(object.type + "-" + object.name)
-        
-            trans, rot, scale = object.matrix_local.decompose()
-
-            rot = rot.to_euler()
-
-            rot.x = math.degrees(rot.x)
-            rot.y = math.degrees(rot.y)
-            rot.z = math.degrees(rot.z)
-
-            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
-            print("Rot(%f,%f,%f)" % (rot.x,rot.y,rot.z))
-            print("Scale(%f,%f,%f)" % (scale.x,scale.y,scale.z))
-
-            if object.parent:
-                print("Parent: " + object.parent.name)
-            print()
-
-
+         self.export()
 
          print("シーン情報をExportしました")
          self.report({'INFO'},"シーン情報をExportしました")
